e77e4aa589909afac62aa61a0cfbb6e214cb8532
Anton1985 <arosasj1985@gmail.com>
2015-04-14 23:55:15 -0500

    agregando parametro tipe a Objeto

---
+++ Proy002/db/migrate/20150415044615_add_tipe_objeto.rb
@@ -0,0 +1,4 @@
+class AddTipeObjeto < ActiveRecord::Migration
+  def change
+  end
+end
--- Proy002/db/schema.rb
+++ Proy002/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150415014738) do
+ActiveRecord::Schema.define(version: 20150415044615) do
 
   create_table "objetos", force: :cascade do |t|
     t.integer  "padreid"
